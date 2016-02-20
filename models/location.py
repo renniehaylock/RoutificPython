4e477eef15aca473301d97a7a5acd81ffa50c9f7
Andre Soesilo <andre@axiomzen.co>
2016-02-20 11:16:13 +0800

    Fix vehicle constructor with correct parameters

--- models/location.py
+++ models/location.py
@@ -1,5 +1,6 @@
 class Location(object):
-  def __init__(self,lat,lng,name=None):
+  def __init__(self,lat,lng,id=None,name=None):
+    self.id = id
     self.name   = name
     self.lat    = lat
     self.lng    = lng
--- models/vehicle.py
+++ models/vehicle.py
@@ -1,9 +1,15 @@
 from location import Location
 class Vehicle(object):
-    def __init__(self, lat, lng, name = None, start = None, end = None, duration = None, load = None, type = None):
-        self.location = Location(lat, lng, name)
-        self.start = start
-        self.end = end
-        self.duration = duration
-        self.load = load
+    def __init__(self, startId, startLat, startLng, startName = None, endId, endLat, endLng, endName = None, shiftStart = None, shiftEnd = None, capacity = None, speed = None, type = None, strictStart = None, minVisits = None, breakStart = None, breakEnd = None, breakDuration = None):
+        self.startLocation = Location(startLat, startLng, startId, startName)
+        self.endLocation = Location(endLat, endLng, endId, endName)
+        self.shiftStart = shiftStart
+        self.shiftEnd = shiftEnd
+        self.capacity = capacity
+        self.speed = speed
         self.type = type
+        self.strictStart = strictStart
+        self.minVisits = minVisits
+        self.breakStart = breakStart
+        self.breakEnd = breakEnd
+        self.breakDuration = breakDuration
