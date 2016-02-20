98bfdae97457065e195b73e14484c17f5ce8cc8d
Rennie Haylock <renniehaylock@gmail.com>
2016-02-19 19:22:44 -0800

    Serializing Visit into Json

--- models/location.py
+++ models/location.py
@@ -1,5 +1,23 @@
 class Location(object):
+  '''
+  (String)      name
+  (Latitude)    lat
+  (Longitude)   lng
+  '''
   def __init__(self,lat,lng,name=None):
+    # TODO: Type Checking for lat and lon
     self.name   = name
     self.lat    = lat
     self.lng    = lng
+
+  '''
+  Serialize object to dictionary
+  '''
+  def toDict(self):
+    location = {}
+
+    if self.name: location["name"] = self.name
+    if self.lat:  location["lat"] = self.lat
+    if self.lng:  location["lng"] = self.lng
+
+    return location
--- models/visit.py
+++ models/visit.py
@@ -1,10 +1,37 @@
 from location import Location
+import json
 
 class Visit(object):
+  '''
+  (String(hh:mm))     start
+  (String(hh:mm))     end 
+  (Number (minutes))  duration
+  (Number (any unit)) load
+  (String or Array)   type
+  '''
   def __init__(self,lat,lng,name=None,start=None,end=None,duration=None,load=None,type=None):
+    # TODO: Type check parameters
     self.location = Location(lat,lng,name)
     self.start    = start
     self.end      = end
     self.duration = duration
     self.load     = load
     self.type     = type
+
+  '''
+  Serialize object to dictionary
+  '''
+  def toDict(self):
+    visit = {}
+
+    if self.location: visit["location"] = self.location.toDict()
+    if self.start:    visit["start"] = self.start
+    if self.end:      visit["end"] = self.end
+    if self.duration: visit["duration"] = self.duration
+    if self.load:     visit["load"] = self.load
+    if self.type:     visit["type"] = self.type
+
+    return visit
+
+  def toJson(self):
+    return json.dumps(self.toDict())
